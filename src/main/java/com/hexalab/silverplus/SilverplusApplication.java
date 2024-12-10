5dc6731332c9444469fcea70da3e56287c0c2976
SangMoo Lee <dltkdan7373@hanmail.net>
2024-12-10 19:31:05 +0900

    test

---
+++ src/main/java/com/hexalab/silverplus/SilverplusApplication.java
@@ -0,0 +1,13 @@
+package com.hexalab.silverplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hexalab/silverplus/SilverplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexalab.silverplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
